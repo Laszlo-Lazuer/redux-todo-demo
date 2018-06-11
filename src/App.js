22dd9433b26c17eca1b2bc8fa5572a7ffee8bc57
Laszlo-Lazuer <visualjazz@gmail.com>
2018-06-11 05:43:33 -0700

    update - stub out the post form component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import PostForm from './components/PostForm.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
-        {/* 
         <PostForm /> 
+        {/* 
         <AllPost />        
         */}
       </div>
