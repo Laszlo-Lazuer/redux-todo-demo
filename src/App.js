414d245eb87584239ff77d129cd3041b68b86164
Laszlo-Lazuer <visualjazz@gmail.com>
2018-06-11 05:36:12 -0700

    Updates the project to include components.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hello</h1>
+        {/* 
+        <PostForm /> 
+        <AllPost />        
+        */}
       </div>
     );
   }
